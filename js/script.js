c0769ef3a15c93c841ccbc01c6ee97a2cedf86ac
xxxZino <egasaputraaa12@students.amikom.ac.id>
2024-10-16 22:16:15 +0700

    Bioskop

---
+++ js/script.js
@@ -0,0 +1,17 @@
+// toggle class active
+
+const navbarNav = document.querySelector('.navbar-nav');
+
+// ketika menu di click
+document.querySelector('#menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+};
+
+// click di luar side bar
+const menu = document.querySelector('#menu');
+
+document.addEventListener('click',  function(e){
+    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
+        navbarNav.classList.remove('active')
+    }
+});

6e781066b0a8b709c71cc4a141de10b8873a4a04
xxxZino <egasaputraaa12@students.amikom.ac.id>
2024-10-29 11:42:43 +0700

    update

---
+++ js/main.js
@@ -0,0 +1,48 @@
+let swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+});
+
+
+$('.owl-carousel').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:true,
+    responsive:{
+        0:{
+            items:2
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
+
+
+
+
+
+
+const nav = document.querySelector(".nav-items")
+const open = document.getElementById("open")
+const close = document.getElementById("close")
+
+open.addEventListener("click", () => {
+    nav.style.display="flex";
+    nav.style.top="0%"
+})
+close.addEventListener("click", () => {
+    nav.style.top="-110%"
+})
--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
 // toggle class active
 
-const navbarNav = document.querySelector('.navbar-nav');
+const navbarNav = document.querySelector(".navbar-nav");
 
 // ketika menu di click
-document.querySelector('#menu').onclick = () => {
-    navbarNav.classList.toggle('active');
+document.querySelector("#menu").onclick = () => {
+  navbarNav.classList.toggle("active");
 };
 
 // click di luar side bar
-const menu = document.querySelector('#menu');
+const menu = document.querySelector("#menu");
 
-document.addEventListener('click',  function(e){
-    if(!menu.contains(e.target) && !navbarNav.contains(e.target)){
-        navbarNav.classList.remove('active')
-    }
+document.addEventListener("click", function (e) {
+  if (!menu.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+document.getElementById('home').addEventListener('click', function() {
+  window.location.href = 'home.html';
+});
+document.getElementById('hl').addEventListener('click', function() {
+  window.location.href = 'home.html';
 });
